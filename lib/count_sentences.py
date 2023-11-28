70b908af58c9972067b7c28abbd7f86b78dfb257
lorna <lexycarrington9@gmail.com>
2023-11-28 12:26:36 +0300

    completed challenge

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,24 @@
 #!/usr/bin/env python3
-
 class MyString:
-  pass
+    def _init_(self, value=''):
+        if not isinstance(value, str):
+            raise ValueError("The value must be a string.")
+        self.value = value
+
+    def is_sentence(self):
+        return self.value.endswith('.')
+
+    def is_question(self):
+        return self.value.endswith('?')
+
+    def is_exclamation(self):
+        return self.value.endswith('!')
+
+    def count_sentences(self):
+        if not isinstance(self.value, str):
+            raise AssertionError("The value must be a string.")
+        
+        punctuations = ['.', '!', '?']
+        modified_str = self.value.replace('!!', '.').replace('?', '.').replace('!', '.')
+        sentences = [sentence for sentence in modified_str.split('.') if sentence.strip()]
+        return len(sentences)
